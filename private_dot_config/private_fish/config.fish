5cea7353b7fcf6a396d7578faf439abebf585229
luqmanishere <luqmanulhakim1720@gmail.com>
2024-01-04 09:39:11 +0800

    fish: add aliases to eza

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,6 +1,20 @@
 # add cargo bin path
 fish_add_path "$HOME/.cargo/bin"
 
+# aliases
+if type -q "eza"
+    alias ls="eza --git --group-directories-first --time-style=long-iso"
+    alias l="ls -1"
+    alias sl="ls"
+    alias ll="ls -al"
+    alias la="ls -lbhHigUmuSa"
+    alias tree="eza --tree"
+end
+
+alias ip="ip --color=auto"
+
+alias tm="tmux attach -t main || tmux new -s main";
+
 # set default editor to nvim
 set -Ux EDITOR "nvim"
 if status is-interactive
