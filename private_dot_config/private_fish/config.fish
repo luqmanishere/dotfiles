0389b4412b052e10e18473bdfb5bd8cc058afcdd
luqmanishere <luqmanulhakim1720@gmail.com>
2024-01-04 09:21:01 +0800

    fish: add keychain

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -5,6 +5,8 @@ fish_add_path "$HOME/.cargo/bin"
 set -Ux EDITOR "nvim"
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    # keychain --eval $SSH_KEYS_TO_AUTOLOAD | source
+    keychain --eval gitmain | source
     zoxide init fish | source
     starship init fish | source
 end
